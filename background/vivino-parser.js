ed6b7612d271062c0a7ea67fa1b80b431618ae6e
leftyy <ustyantsev.makar@mail.ru>
2024-02-17 02:41:24 +0300

    added vivino parser

---
+++ background/vivino-parser.js
@@ -0,0 +1,40 @@
+const unirest = require("unirest")
+const cheerio = require("cheerio");
+const getLinks = async(url) => {
+    try{
+        const response = await unirest.get(url)
+        const data = response.body
+        const $ = cheerio.load(data)
+
+        let hrefs = []
+        $("a").each((index, value) => {
+            let href = $(value).attr("href")
+            hrefs.push(href)
+        })
+        let validHrefs = []
+        for (let i = 0; i < hrefs.length; i++) {
+            // wouldnt work without this conversion for some reason
+            // (TypeError: Cannot read properties of undefined (reading 'startsWith'))
+            let value = String(hrefs[i])
+            if (value.startsWith("/wines/")){
+                validHrefs.push(value.toString());
+            }
+        }
+
+        let links = []
+
+        for (const href of validHrefs) {
+            links.push(`https://www.vivino.com${href}`)
+        }
+
+        return links;
+    }
+    catch(e)
+    {
+        console.log(e);
+    }
+}
+
+
+
+export default getLinks()
